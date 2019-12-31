159fd01ec2696d280cd2a5fbdf32824e2f289b86
tksrl0379 <tksrl0379@naver.com>
2019-12-31 13:23:27 +0900

    첫 commit

---
+++ OutOfKiosk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OutOfKiosk
+//
+//  Created by a1111 on 2019/12/31.
+//  Copyright © 2019 OOK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
