cd5e50784a541812e983b2443a1db96b24a440e0
sim <bwrw2347@naver.com>
2020-01-03 16:12:57 +0900

    DialogueFlow 올리기 위한 view 생성

---
+++ Controller/DialogueFlowPopUpController.swift
@@ -0,0 +1,22 @@
+//
+//  DialogueFlowPopUpController.swift
+//  OutOfKiosk
+//
+//  Created by a1111 on 2020/01/03.
+//  Copyright © 2020 OOK. All rights reserved.
+//
+
+import UIKit
+
+class DialogueFlowPopUpController: UIViewController{
+    
+    override func viewDidLoad() {
+           super.viewDidLoad()
+    }
+    
+    @IBAction func close_Btn(_ sender: Any) {
+        self.presentingViewController?.dismiss(animated: true, completion: nil)
+        
+        
+    }
+}
