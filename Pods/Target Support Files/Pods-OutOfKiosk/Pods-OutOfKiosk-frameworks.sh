00e1c8111b2d6a8a18583bb13699f4d4e15453c6
tksrl0379 <tksrl0379@naver.com>
2020-01-15 00:48:16 +0900

    DialogPopUpController UI 변경(1. 녹음 중 / 녹음 전 을 알리는 애니메이션 추가 2. 폰트 변경 3. back button 변경 )

--- Pods/Target Support Files/Pods-OutOfKiosk/Pods-OutOfKiosk-frameworks.sh
+++ Pods/Target Support Files/Pods-OutOfKiosk/Pods-OutOfKiosk-frameworks.sh
@@ -163,10 +163,12 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ApiAI/ApiAI.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/lottie-ios/Lottie.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ApiAI/ApiAI.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/lottie-ios/Lottie.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
