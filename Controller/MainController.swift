b8ca41568d38c93347e5ff52385c8923c7742eeb
sim <bwrw2347@naver.com>
2020-01-29 11:16:39 +0900

    UI 수정

--- Controller/MainController.swift
+++ Controller/MainController.swift
@@ -10,19 +10,21 @@ import UIKit
 
 class MainController : UIViewController{
     
-    @IBOutlet weak var title_View: UIView!
+    private var userId: String?
     
+    @IBOutlet weak var title_View: UIView!
+    @IBOutlet weak var sub_View: UIView!
+    @IBOutlet weak var sub2_View: UIView!
+    @IBOutlet weak var progressBar_view: UIView!
     
     @IBOutlet weak var storeSelect_Btn: UIButton!
-  
-    @IBOutlet weak var id_Label: UILabel!
-    
     @IBOutlet weak var favorite_Btn: UIButton!
     
     
+    //@IBOutlet weak var id_Label: UILabel!
+    @IBOutlet weak var purchaseCount_Label: UILabel!
+    @IBOutlet weak var grade_Label: UILabel!
     
-    
-    @IBOutlet weak var sub_View: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,18 +49,62 @@ class MainController : UIViewController{
         sub_View.layer.shadowOffset = .zero
         sub_View.layer.shadowRadius = 4
         sub_View.layer.cornerRadius = 5
+        	
+        sub2_View.layer.shadowColor = UIColor.black.cgColor
+        sub2_View.layer.shadowOpacity = 0.3
+        sub2_View.layer.shadowOffset = .zero
+        sub2_View.layer.shadowRadius = 4
+        sub2_View.layer.cornerRadius = 5
         
         storeSelect_Btn.layer.cornerRadius = 5
         
         favorite_Btn.layer.cornerRadius = 5
         
-        let userId = UserDefaults.standard.string(forKey: "id")!
         
-        id_Label.text = userId
+        let cp = CircularProgressView(frame: CGRect(x: 35.5, y: 16.5, width: 120.0, height: 120.0))
+        cp.trackColor = UIColor(red: 188.0/255.0, green: 188.0/255.0, blue: 188.0/255.0, alpha: 0.3)
+        cp.progressColor = UIColor.systemYellow
+        cp.tag = 101
+        self.sub2_View.addSubview(cp)
+        //cp.center = self.progressBar_view.center
+        
+        
+        self.perform(#selector(animateProgress), with: nil, afterDelay: 1.0)
+        
+        //CircularProgress.trackColor = UIColor.white
+        //CircularProgress.progressColor = UIColor.purple
+        //CircularProgress.setProgressWithAnimation(duration: 1.0, value: 0.3)
+        
+        
+        userId = UserDefaults.standard.string(forKey: "id")!
+        
+        //id_Label.text = userId! + " 님은"
+        
+        self.getPurchaseCount(){
+            responseString in
+            DispatchQueue.main.async {
+                self.purchaseCount_Label.text = (responseString! as String) as String + " / 25"
+            }
+        }
+        
+        grade_Label.text = "Bronze"
         
         
         
     }
+    
+    
+    @objc func animateProgress() {
+        let cP = self.view.viewWithTag(101) as! CircularProgressView
+        cP.setProgressWithAnimation(duration: 0.7, value: 0.3)
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
     
     func addShadow(btn : UIButton){
         btn.layer.shadowColor = UIColor.black.cgColor
@@ -68,6 +114,43 @@ class MainController : UIViewController{
         
         btn.layer.cornerRadius = 1
     }
+    
+    func getPurchaseCount(handler: @escaping (_ responseStrng : NSString?) -> Void){
+        /* php 통신 */
+        let request = NSMutableURLRequest(url: NSURL(string: "http://ec2-13-124-57-226.ap-northeast-2.compute.amazonaws.com/getUserInfo.php")! as URL)
+        request.httpMethod = "POST"
+        
+//        let postString = "name=\(self.name!)&size=\(self.size!)&count=\(self.count!)"
+        let postString = "id=\(self.userId!)"
+        print(self.userId!)
+        request.httpBody = postString.data(using: String.Encoding.utf8)
+        
+        
+        /* URLSession: HTTP 요청을 보내고 받는 핵심 객체 */
+        let task = URLSession.shared.dataTask(with: request as URLRequest) {
+            data, response, error in
+            
+            print("response = \(response!)")
+            
+            /* php server에서 echo한 내용들이 담김 */
+            var responseString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
+            
+            /* php서버와 통신 시 NSString에 생기는 개행 제거 */
+            responseString = responseString?.trimmingCharacters(in: .newlines) as NSString?
+            
+            print("responseString = \(responseString!)")
+            
+            /* UI 변경은 메인쓰레드에서만 가능 */
+            
+            //self.speechAndText(textResponse + " 총 \(responseString!)원입니다. 주문하시겠습니까 ?")
+            handler(responseString)
+            
+        }
+        task.resume()
+    }
+    
+    
+    
     
     
     /* 카페 버튼 */
