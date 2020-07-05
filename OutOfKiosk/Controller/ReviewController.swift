8690674cd652f6d34703659a9c6de80165b0f026
tksrl0379 <tksrl0379@naver.com>
2020-07-05 23:35:50 +0900

    ReViewController 리팩토링 - 변경한 주석 및 코딩 규칙 적용 - 중복되거나 잘못된 UI코드 수정 - 스토리보드 오류 메시지 해결

--- OutOfKiosk/Controller/ReviewController.swift
+++ OutOfKiosk/Controller/ReviewController.swift
@@ -10,68 +10,70 @@ import UIKit
 
 class ReviewController : UIViewController, UITableViewDelegate, UITableViewDataSource{
     
-    @IBOutlet weak var reviewWrite_Btn: UIButton!
-    
+    // MARK: - Propery
+    // MARK: Custom Property
     var storeEnName : String?
     var storeKorName: String?
-    var reviewUserId: Array<String>? = []
-    var reviewContents: Array<String>? = []
-    var reviewTime: Array<String>? = []
-    var reviewRating: Array<Double>? = []
+    var reviewUserId: Array<String> = []
+    var reviewContents: Array<String> = []
+    var reviewTime: Array<String> = []
+    var reviewRating: Array<Double> = []
     
+    // MARK: IBOutlet
+    @IBOutlet weak var reviewWrite_Btn: UIButton!
     @IBOutlet weak var reviewTableView: UITableView!
-
     
-    @IBAction func reviewWriteButton(_ sender: Any) {
-        guard let rvc = self.storyboard?.instantiateViewController(withIdentifier: "ReviewWriteController") as? ReviewWriteController else { return }
-        
-        rvc.storeEnName = self.storeEnName
-        self.navigationController?.pushViewController(rvc, animated: true)
-    }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return reviewUserId!.count
-    }
-    
-
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//      재사용할 수 있는 cell을 ProductTableView에 넣는다는 뜻. UITableViewCell을 반환하기 때문에 Reviewlist로 다운캐스팅
-        
-        let cell = reviewTableView.dequeueReusableCell(withIdentifier: "ReviewList", for: indexPath ) as! ReviewList
-        
-        cell.reviewUserId_Label.text = reviewUserId![indexPath.row] + "님"
-        cell.reviewContents_Label.text = reviewContents![indexPath.row]
-        cell.reviewTime_Label.text = reviewTime![indexPath.row]
-        
-        // 별점
-        // Reset float rating view's background color
-        cell.floatRatingView.backgroundColor = UIColor.clear
-
-        //cell.floatRatingView.delegate = self
-        cell.floatRatingView.contentMode = UIView.ContentMode.scaleAspectFit
-        cell.floatRatingView.type = .halfRatings
-        cell.floatRatingView.editable = false
-        cell.floatRatingView.rating = reviewRating![indexPath.row]
-        
-        
-
-        return cell
-    }
-    
-  
-    
-    @objc func buttonAction(_ sender: UIBarButtonItem) {
-      self.navigationController?.popViewController(animated: true)
-    }
-    
+    // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.initializeNavigationItem()
+        self.initializeView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        self.setUpReviewContents()
+    }
+    
+    
+    // MARK: - Method
+    // MARK: TableView
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return reviewUserId.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = reviewTableView.dequeueReusableCell(withIdentifier: "ReviewList", for: indexPath ) as! ReviewList
+        
+        cell.reviewUserId_Label.text = reviewUserId[indexPath.row] + "님"
+        cell.reviewContents_Label.text = reviewContents[indexPath.row]
+        cell.reviewTime_Label.text = reviewTime[indexPath.row]
+        
+        // 별점
+        cell.floatRatingView.backgroundColor = UIColor.clear
+        
+        cell.floatRatingView.contentMode = UIView.ContentMode.scaleAspectFit
+        cell.floatRatingView.type = .halfRatings
+        cell.floatRatingView.editable = false
+        cell.floatRatingView.rating = reviewRating[indexPath.row]
+        
+        return cell
+    }
+    
+    // MARK: Custom Method
+    func initializeNavigationItem() {
+        self.navigationItem.title = "리뷰하기"
         self.navigationItem.leftBarButtonItem = BackButton(controller: self)
         self.navigationItem.leftBarButtonItem?.accessibilityLabel = self.storeKorName! + " 뒤로가기"
+    }
+    
+    func initializeView() {
         
-        /* 테두리 둥글게 만들기 */
+        // 테두리 둥글게 만들기
         reviewWrite_Btn.layer.cornerRadius = 5
         reviewWrite_Btn.layer.borderWidth = 0.2
         reviewWrite_Btn.layer.borderColor = UIColor.gray.cgColor
@@ -85,69 +87,51 @@ class ReviewController : UIViewController, UITableViewDelegate, UITableViewDataS
         // 테이블
         reviewTableView.delegate = self
         reviewTableView.dataSource = self
-        // tableView의 계산된 높이 값은 68이다. 즉 Default Height이다.
-        reviewTableView.estimatedRowHeight = 100.0
-        // tableView의 rowHeight는 유동적일 수 있다
-        reviewTableView.rowHeight = UITableView.automaticDimension
-        
-        
-        
-        
-        
+        //reviewTableView.estimatedRowHeight = 100.0
+        reviewTableView.rowHeight = UITableView.automaticDimension // 테이블의 rowHeight 유동적으로 설정
     }
     
-    override func viewWillAppear(_ animated: Bool) {
+    func setUpReviewContents() {
         
-        /* TableView 제목 사이즈 동적 조절 */
-        self.navigationController?.navigationBar.prefersLargeTitles = false
-        self.navigationItem.title = "리뷰하기"
+        // 리뷰 정보 받아오기
+        reviewUserId.removeAll()
+        reviewContents.removeAll()
+        reviewTime.removeAll()
+        reviewRating.removeAll()
         
-        /* 리뷰 정보 받아오기 */
-        reviewUserId?.removeAll()
-        reviewContents?.removeAll()
-        reviewTime?.removeAll()
-        reviewRating?.removeAll()
-        
-        CustomHttpRequest().phpCommunication(url: "getReviewInfo.php", postString: "storeEnName=\(storeEnName!)"){
+        CustomHttpRequest().phpCommunication(url: "getReviewInfo.php", postString: "storeEnName=\(storeEnName!)") {
             responseString in
             
-            guard let dict = CustomConvert().convertStringToDictionary(text: responseString as! String) else {return}
-        
-            for i in 1...dict.count{
+            guard let dict = CustomConvert().convertStringToDictionary(text: responseString ) else { return }
+            
+            for i in 1...dict.count {
                 let dict = dict[String(i)] as! NSDictionary
-                self.reviewUserId?.append(dict["userId"] as! String)
-                self.reviewContents?.append(dict["contents"] as! String)
-                self.reviewTime?.append(dict["time"] as! String)
-                self.reviewRating?.append(dict["rating"] as! Double)
-                
+                self.reviewUserId.append(dict["userId"] as! String)
+                self.reviewContents.append(dict["contents"] as! String)
+                self.reviewTime.append(dict["time"] as! String)
+                self.reviewRating.append(dict["rating"] as! Double)
             }
             
             DispatchQueue.main.async {
                 self.reviewTableView.reloadData()
             }
         }
-        
-        /* navigationbar 투명 설정 */
-        //self.navigationController!.navigationBar.setBackgroundImage(UIImage(), for: .default)
-        //self.navigationController!.navigationBar.shadowImage = UIImage()
-        
     }
     
+    @objc func buttonAction(_ sender: UIBarButtonItem) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    // MARK: IBAction
+    @IBAction func reviewWriteButton(_ sender: Any) {
+        guard let rvc = self.storyboard?.instantiateViewController(withIdentifier: "ReviewWriteController") as? ReviewWriteController else { return }
+        
+        rvc.storeEnName = self.storeEnName
+        self.navigationController?.pushViewController(rvc, animated: true)
+    }
     
 }
 
 
-extension ReviewController: FloatRatingViewDelegate {
 
-    // MARK: FloatRatingViewDelegate
-    
-    func floatRatingView(_ ratingView: FloatRatingView, isUpdating rating: Double) {
-        //liveLabel.text = String(format: "%.2f", self.floatRatingView.rating)
-    }
-    
-    func floatRatingView(_ ratingView: FloatRatingView, didUpdate rating: Double) {
-        //updatedLabel.text = String(format: "%.2f", self.floatRatingView.rating)
-    }
-    
-}
 
